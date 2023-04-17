dcc6f0591a0a4b876e74d505c7da7b31bcce8c73
Durgalaks <durgalakshmi.perumal@fssa.freshworks.com>
2023-04-17 16:38:14 +0530

    feat: completed the buy crud

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,403 @@ const musicbox = [
 
   localStorage.setItem("music-card", JSON.stringify(musicbox));
   
-  
+  let booktoread =[
+         {
+          id: "01",
+          img: "./../assets/images/adventure.jpeg",
+          alt: "book-img",
+          bok_link: "./../assets/images/durga.pdf",
+          book_name: "gendral knowledage",
+          section:"adventure"
+       },
+       {
+        id: "02",
+        img: "./../assets/images/adventure1.jpeg",
+        alt: "book-img",
+        bok_link: "./../assets/images/durga.pdf",
+        book_name: "gendral knowledage",
+        section:"adventure"
+     }, 
+     {
+      id: "03",
+      img: "./../assets/images/adventure2.jpeg",
+      alt: "book-img",
+      bok_link: "./../assets/images/durga.pdf",
+      book_name: "gendral knowledage",
+      section:"adventure"
+   }, 
+   {
+    id: "04",
+    img: "./../assets/images/adventure3.jpeg",
+    alt: "book-img",
+    bok_link: "./../assets/images/durga.pdf",
+    book_name: "gendral knowledage",
+    section:"adventure"
+ },
+ {
+  id: "05",
+  img: "./../assets/images/triller.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"triller"
+},
+{
+  id: "06",
+  img: "./../assets/images/triller1.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"triller"
+},
+{
+  id: "07",
+  img: "./../assets/images/triller2.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"triller"
+},
+{
+  id: "08",
+  img: "./../assets/images/triller3.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"triller"
+},
+{
+  id: "09",
+  img: "./../assets/images/romanric.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"romantic"
+},
+{
+  id: "010",
+  img: "./../assets/images/romantic.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"romantic"
+},
+{
+  id: "011",
+  img: "./../assets/images/romantic1.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"romantic"
+},
+{
+  id: "012",
+  img: "./../assets/images/romantic2.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"romantic"
+},
+{
+  id: "013",
+  img: "./../assets/images/comedy.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"comedy"
+},
+{
+  id: "014",
+  img: "./../assets/images/comedy1.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"comedy"
+},
+{
+  id: "015",
+  img: "./../assets/images/comedy2.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"comedy"
+},
+{
+  id: "016",
+  img: "./../assets/images/comedy3.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"comedy"
+},
+{
+  id: "017",
+  img: "./../assets/images/classic.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"classic"
+},
+{
+  id: "018",
+  img: "./../assets/images/classic1.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"classic"
+},
+{
+  id: "019",
+  img: "./../assets/images/classic2.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"classic"
+},
+{
+  id: "020",
+  img: "./../assets/images/classic3.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"classic"
+},
+{
+  id: "021",
+  img: "./../assets/images/horror.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"horror"
+},
+{
+  id: "022",
+  img: "./../assets/images/horror1.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"horror"
+},
+{
+  id: "023",
+  img: "./../assets/images/horror2.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"horror"
+},
+{
+  id: "024",
+  img: "./../assets/images/horror3.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"horror"
+},
+{
+  id: "025",
+  img: "./../assets/images/devotional.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"devotional"
+},
+{
+  id: "026",
+  img: "./../assets/images/devotional1.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"devotional"
+},
+{
+  id: "027",
+  img: "./../assets/images/devotional2.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"devotional"
+},
+{
+  id: "028",
+  img: "./../assets/images/devotional3.jpeg",
+  alt: "book-img",
+  bok_link: "./../assets/images/durga.pdf",
+  book_name: "gendral knowledage",
+  section:"devotional"
+},
+  ]
+  localStorage.setItem("readbooks",JSON.stringify(booktoread));
+  ;
+
+
+  const booktobuy = [
+    {
+      
+      id: "029",
+      book_name: "To Kill a Mockingbird",
+      book_price: "$10.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"adventure"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"triller"
+
+    },
+    { id: "031",
+      book_name: "Pride and Prejudice",
+      book_price: "$12.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"romantic"
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"triller"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"adventure"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"triller"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"adventure"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"romantic"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"romantic"
+
+    }, 
+     {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"comedy"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"comedy"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"comedy"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"classic"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"horror"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"classic"
+
+    },
+      {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"classic"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"horror"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"horror"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"devotional"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"devotional"
+
+    },
+    {
+      id: "030",
+      book_name: "1984",
+      book_price: "$8.99",
+      book_img: "./../assets/images/devotional3.jpeg",
+      section:"devotional"
+
+    },
+  ];
+  localStorage.setItem("formtoaddbuybooks",JSON.stringify(booktobuy));
